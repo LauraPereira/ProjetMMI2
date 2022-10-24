43e3953f7419d5ecd9aa1df1f62bbd9b6fde9341
Laura Pereira <37901764+LauraPereira@users.noreply.github.com>
2022-10-24 15:50:44 +0200

    Update 18mmi2pj06.sql

--- bdd/18mmi2pj06.sql
+++ bdd/18mmi2pj06.sql
@@ -17,7 +17,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Base de données :  `18mmi2pj06`
+-- Base de données :  `ProjetMMI2`
 --
 
 -- --------------------------------------------------------
